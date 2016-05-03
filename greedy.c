24cb8d568c17b8c19439fa81877816777e911f32
Alex <Alehandr0@users.noreply.github.com>
2016-05-03 13:01:45 +0300

    Create greedy.c

---
+++ greedy.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <cs50.h>
+#include <stdio.h>
+
+// Функция получает количество центов и возвращает количество монет
+int coins(penny)
+{
+    int ostatok, monet=0;
+    for (int i = 6; i <= 9; i++)
+    {
+        int nominal;
+        switch (i)
+        {
+            case 6: nominal = 25; break;
+            case 7: nominal = 10; break;
+            case 8: nominal = 5; break;
+            case 9: nominal = 1; break;
+        }
+     
+        ostatok = penny % nominal;
+        monet += (penny - penny % nominal) / nominal;
+        penny = ostatok;
+    }
+    return monet;
+}
+
+int main(void)
+{
+// Получаем от пользователя параметр количества сдачи
+    float money;
+    int penny;
+    do
+    {
+        printf("How much change is owed? ");
+        money = GetFloat() * 10000.000;
+        penny = floor(floor(money) / 100);
+    }
+    while (penny < 1);
+
+    printf("%i\n", coins(penny));
+}
+/**
+* Результат автоматической проверки находится по адресу:
+* https://sandbox.cs50.net/checks/e45bc3db9b424b80a548964ba9ca28c3
+* 
+*/
