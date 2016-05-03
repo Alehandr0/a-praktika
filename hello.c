418d728716cb5f487908f92bf29bc7af226531de
Alex <Alehandr0@users.noreply.github.com>
2016-05-03 12:53:19 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
-# cs50
+#include <stdio.h>
 
 int main(void)
-{
-  printr("Hello, world !!!");
+{ 
+    printf("hello, world\n"); 
 }
+/**
+* Результат автоматической проверки находится по адресу:
+* https://sandbox.cs50.net/checks/ff8f583d2a914fb8893978b2df88e3b2
+* 
+*/
