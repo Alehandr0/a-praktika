1ffec22e248ee493890d3fa1a7788647902654af
Alex <Alehandr0@users.noreply.github.com>
2016-05-06 22:37:02 +0300

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -7,12 +7,9 @@
 char letter(int b, int key)
 {
     int dop=90;
-    if (islower(b))
-        dop=122;
-        
+    if (islower(b)) dop=122;
     b = b + key;
-    if (b > dop)
-        b -= 26;
+    if (b > dop) b -= 26;
     return b;
 }
 
@@ -23,34 +20,15 @@ int main(int n, char* ar[])
         printf("Запуск программы должен быть с параметром. К примеру, как в кавычках \"./caesar 71\"\n");
         return 1;
     }
-    int key = 0, a = strlen(ar[1]);
-    for (int i = a - 1; i >= 0; i--)
-    {
-        char pr = ar[1][i];
-        if (isdigit(pr))
-        {
-            key += pow(10, a - i - 1) * (ar[1][i] - 48);
-        }
-        else
-        {
-            key = 0;
-            break;
-        }
-    }
-    key = key % 26;
-    printf("Ключ = %i\n", key);
     
-    
-   
+    int key = atoi(argv[1]) % 26;
     string s = GetString();
     if (s != NULL)
     {
-       for(int i = 0, phrase = strlen(s); i < phrase; i++)
+        for(int i = 0, phrase = strlen(s); i < phrase; i++)
         {
-        if (isalpha(s[i]))
-            s[i] = letter(s[i], key);
-
-        printf("%c", s[i]);
+            if (isalpha(s[i])) s[i] = letter(s[i], key);
+            printf("%c", s[i]);
         }
         printf("\n");
     }
