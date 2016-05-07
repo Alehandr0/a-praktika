a961aef441e94383a9e5d71edbcc4ac8c942101d
Alex <Alehandr0@users.noreply.github.com>
2016-05-07 14:43:06 +0300

    Create vigenere.c

---
+++ vigenere.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Пртотип кодировщика
+char letter(int b, int key);
+
+int main(int n, char* argv[])
+{
+    if (n != 2)
+    {
+        printf("Запуск программы должен быть с параметром. К примеру, как в кавычках \"./caesar 71\"\n");
+        return 1;
+    }
+
+    int key = atoi(argv[1]) % 26;
+    string s = GetString();
+    if (s != NULL)
+    {
+        for(int i = 0, phrase = strlen(s); i < phrase; i++)
+        {
+// Далее проверяется каждая буква пользовательского ввода и только буквы отправляются на кодировку
+            if (isalpha(s[i])) s[i] = letter(s[i], key);
+            printf("%c", s[i]);
+        }
+        printf("\n");
+    }
+}
+
+// Эта функция получает ASCII буквы и ключ, результатом возвращает закодированную букву
+char letter(int b, int key)
+{
+    int dop=90;
+    if (islower(b)) dop=122;
+    b = b + key;
+    if (b > dop) b -= 26;
+    return b;
+}
+
+/**
+Результат автоматической проверки:
+check50 2014.fall.pset2.vigenere vigenere.c
+находится по адресу:
+
+*/
