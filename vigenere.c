5e8a7a39de29fbdc837aca313b7a358eac751050
Alex <Alehandr0@users.noreply.github.com>
2016-05-07 22:50:34 +0300

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -5,33 +5,60 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Пртотип кодировщика
-char letter(int b, int key);
+#define L 26
+// Далее константа равнвна значению ASCII первого символа алфавита, так как в метка-1 используется функция toupper()
+#define Bascii 65
 
-int main(int n, char* argv[])
+// Прототип
+char caesar(int b, int key);
+
+int main(int n, char* w[])
 {
+
     if (n != 2)
     {
-        printf("Запуск программы должен быть с параметром. К примеру, как в кавычках \"./caesar 71\"\n");
+        printf("Запуск программы должен быть с параметром. К примеру, как в кавычках \"./vigenere WELCOME\"\n");
         return 1;
     }
 
-    int key = atoi(argv[1]) % 26;
-    string s = GetString();
-    if (s != NULL)
+    int len_key = strlen(w[1]) - 1;
+    int key_arr[len_key];
+    printf("%s ,  len_key = %i Проверим, чтоб в ключе были только буквы.\n", w[1], len_key);
+
+// Проверим, чтоб в ключе были только буквы
+    for(int i = 0; i <= len_key; i++)
     {
-        for(int i = 0, phrase = strlen(s); i < phrase; i++)
+// Далее проверяется каждая буква ключа и только если все символы буквы - ключ будет считаться действительным
+        if (!isalpha(w[1][i]))
         {
-// Далее проверяется каждая буква пользовательского ввода и только буквы отправляются на кодировку
-            if (isalpha(s[i])) s[i] = letter(s[i], key);
-            printf("%c", s[i]);
+            printf("Запуск программы должен быть с обязательным параметром в виде строки. К примеру, как в кавычках \"./vigenere TEXT\"\n");
+            return 1;
         }
-        printf("\n");
+        w[1][i] = /* метка-1 */toupper(w[1][i]);
+        key_arr[i] = w[1][i] - Bascii;
+
+        printf("%i ", key_arr[i]);
     }
+    printf("\n");
+
+    string s = GetString();
+    if (s == NULL) return 1;
+
+    int next=0;
+    for(int i = 0, a = strlen(s); i < a; i++)
+    {
+        if (isalpha(s[i]))
+        {
+            s[i] = caesar(s[i], key_arr[next]);
+            next++; if (next > len_key) next = 0;
+        }
+        printf("%c", s[i]);
+    }
+    printf("\n");
 }
 
-// Эта функция получает ASCII буквы и ключ, результатом возвращает закодированную букву
-char letter(int b, int key)
+// Эта функция получает букву и ключ и возвращает закодированную букву
+char caesar(int b, int key)
 {
     int dop=90;
     if (islower(b)) dop=122;
@@ -39,10 +66,9 @@ char letter(int b, int key)
     if (b > dop) b -= 26;
     return b;
 }
-
 /**
 Результат автоматической проверки:
 check50 2014.fall.pset2.vigenere vigenere.c
 находится по адресу:
-
+https://sandbox.cs50.net/checks/af9edaec62644d31b279cbb225126a87
 */
